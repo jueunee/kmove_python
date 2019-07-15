1faadc54f9ac4c22bceb6c6931477568dfc61ce5
jueunee <wndms5144@gmail.com>
2019-07-15 17:45:59 +0900

    20190715마지막

--- basic/test.py
+++ basic/test.py
@@ -1,2 +1,19 @@
 print('hi')
 print("hi")
+food="python's favorite\n \"food\" is perl"
+print(food)
+print('123','한글','영어',sep='-',end='\n')
+print('다음줄임')
+head = "python"
+tail = "is fun!"
+number1=1
+number2=2
+print(head*3)
+print(head+tail)
+print(number1+number2)
+print("=" * 50)
+print(len(food))
+print(food[:5])
+a=6.6666666
+print (' i eat %.8f apples' %a)
+print("python's {1}favor{0}ite".format(12,20))
